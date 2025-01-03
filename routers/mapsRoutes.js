406792863619422b384378506e8dbc57c2a8f03d
Achivers-JustTap <chandus1603@gmail.com>
2025-01-03 10:06:50 -0800

    Updating the Maps

--- routers/mapsRoutes.js
+++ routers/mapsRoutes.js
@@ -4,13 +4,13 @@ const mapController = require('../controllers/mapController');
 const { query } = require('express-validator');
 
 router.get('/get-coordinates',
-    query('address').isString().isLength({ min: 3 }),
+    query('address').isString(),
     mapController.getCoordinates
 );
 
 router.get('/get-distance-time',
-    query('origin').isString().isLength({ min: 3 }),
-    query('destination').isString().isLength({ min: 3 }),
+    query('origin').isString(),
+    query('destination').isString(),
     mapController.getDistanceTime
 )
 
